8d8e3f91c0c6db7973c2206ba19c9bbe13c360bc
burakddemir <77579741+burakddemir@users.noreply.github.com>
2022-11-18 20:32:20 +0200

    Add files via upload

---
+++ forget-pass/script.js
@@ -0,0 +1,23 @@
+function forget()
+{
+    var email= document.getElementById("email").value;
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    if(email=='')
+    {
+        document.getElementById("p1").style.color = "red";
+        document.getElementById("p1").innerHTML = "Please enter your email";
+    }
+    else if (!filter.test(email))
+    {
+        document.getElementById("p1").style.color = "red";
+        document.getElementById("p1").innerHTML = "Invalid email";
+    }
+    else{
+        document.getElementById("p1").style.color = "green";
+        document.getElementById("p1").innerHTML = "Your verefication has been send";
+    }
+}
+
+
+
